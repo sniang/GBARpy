62e3e4d41722bbf21c4e20c231e8938407c216b8
Samuel Niang <samuel.niang@gmail.com>
2021-09-30 15:34:31 +0200

    docs folder

--- src/GBARpy/makedoc.py
+++ src/GBARpy/makedoc.py
@@ -3,4 +3,4 @@ from docstr_md.python import PySoup, compile_md
 
 src_href = Github('https://github.com/sniang/GBARpy/tree/main/src/GBARpy')
 soup = PySoup(path='MCPPicture.py', parser='sklearn', src_href=src_href)
-compile_md(soup, compiler='sklearn', outfile='MCPPicture_doc.md')
+compile_md(soup, compiler='sklearn', outfile='../../docs/MCPPicture_doc.md')
